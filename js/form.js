d3f2f44377ea2497990273d5fae75b6c50f8dfd3
Nicosetzes <nicosetzes@gmail.com>
2023-06-08 12:52:44 -0300

    V1.9 I've added h1 and h2 tags, and a favicon to index. I've also modified some styles, the most important ones were related to :hover on buttons.

--- js/form.js
+++ js/form.js
@@ -1,48 +1,62 @@
+const currentDate = new Date();
+
+const getYear = currentDate.toLocaleString("default", { year: "numeric" });
+const getMonth = currentDate.toLocaleString("default", { month: "2-digit" });
+const getDay = currentDate.toLocaleString("default", { day: "2-digit" });
+
+const dateFormat = getYear + "-" + getMonth + "-" + getDay;
+
+const allDateStart = document.querySelectorAll(".date-start");
+
+allDateStart.forEach((input) => {
+  input.min = dateFormat;
+  input.value = dateFormat;
+});
+
+const allDateEnd = document.querySelectorAll(".date-end");
+
+allDateEnd.forEach((input) => {
+  input.min = dateFormat;
+});
+
 const forms = document.querySelectorAll(".form");
 
 forms.forEach((form) => {
   form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const job = e.target.querySelector(".select-job").value;
-    const location = e.target.querySelector(".select-location").value;
-    const amount = e.target.querySelector(".amount").value;
-    const dateStart = e.target.querySelector(".date-start").value;
-    const dateEnd = e.target.querySelector(".date-end").value;
+    const idOcupacion = e.target.querySelector(".select-job").value;
+    const idProvincia = e.target.querySelector(".select-location").value;
+    const cantidadDeAsegurados = e.target.querySelector(".amount").value;
+    const fechaInicio = e.target.querySelector(".date-start").value;
+    const fechaFin = e.target.querySelector(".date-end").value;
 
-    console.log(job);
-    console.log(location);
-    console.log(amount);
-    console.log(dateStart);
-    console.log(dateEnd);
+    console.log(idOcupacion);
+    console.log(idProvincia);
+    console.log(cantidadDeAsegurados);
+    console.log(fechaInicio);
+    console.log(fechaFin);
 
-    if (!job || !location || !amount || !dateStart || !dateEnd)
-      return Swal.fire({
-        title: "¡Error!",
-        iconColor: "#d30c0c",
-        text: "Por favor, complete todos los datos antes de continuar",
-        icon: "error",
-        confirmButtonText: "OK",
-      });
-    else
-      return Swal.fire({
-        title: "¡Éxito!",
+    // return Swal.fire({
+    //   title: "¡Éxito!",
 
-        html: `
-        <div>Profesión: ${job}</div>
-        <br>
-        <div>Provincia: ${location}</div>
-        <br>
-        <div>Cantidad de asegurados: ${amount}</div>
-        <br>
-        <div>Fecha de inicio: ${dateStart}</div>
-        <br>
-        <div>Fecha de finalización: ${dateEnd}</div>
-        `,
-        icon: "success",
-        confirmButtonText: "OK",
-      });
+    //   html: `
+    //     <div>Profesión: ${job}</div>
+    //     <br>
+    //     <div>Provincia: ${location}</div>
+    //     <br>
+    //     <div>Cantidad de asegurados: ${amount}</div>
+    //     <br>
+    //     <div>Fecha de inicio: ${dateStart}</div>
+    //     <br>
+    //     <div>Fecha de finalización: ${dateEnd}</div>
+    //     `,
+    //   icon: "success",
+    //   confirmButtonText: "OK",
+    // });
 
-    // Llamada GET o POST a la API, con la información capturada en el formulario //
+    // Hago un redirect a Woranz Live, comunicando la información proveniente del formulario a través de query params  //
+
+    // window.location.href = `https://live.woranz.com/ap/inicio?id=252&idProductor=${idProductor}&idOcupacion=${idOcupacion}&idProvincia=${idProvincia}&qty=${cantidadDeAsegurados}&desde=${fechaInicio}&hasta=${fechaFin}`;
   });
 });
