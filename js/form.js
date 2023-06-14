637403ceec9bd6752b4396e28b9fcb08a9abfd2a
Nicosetzes <nicosetzes@gmail.com>
2023-06-14 17:48:01 -0300

    V2.3 I've made some minor changes to styling, and did a little javascript to change target for a tags when accessing from mobile devices.

--- js/form.js
+++ js/form.js
@@ -195,3 +195,12 @@ forms.forEach((form) => {
     window.location.href = `https://live.woranz.com/ap/inicio?id=252&idProductor=${idProductor}&idOcupacion=${idOcupacion}&idProvincia=${idProvincia}&cantidad=${cantidadDeAsegurados}&desde=${fechaInicio}&hasta=${fechaFin}&bd=true`;
   });
 });
+
+// BONUS: Hago que el target de ciertos enlaces NO sea _blank en viewports menores a 768px //
+
+if (window.matchMedia("(max-width: 768px)").matches) {
+  const links = document.querySelectorAll(".link-footer");
+  links.forEach((link) => {
+    link.target = "_self";
+  });
+}
